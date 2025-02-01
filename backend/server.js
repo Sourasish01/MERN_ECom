32fdbe94c841e58eb85c3222e1a1b2231e68229d
Sourasish01 <bhotto2023@gmail.com>
2025-02-02 02:09:11 +0530

    product routes : cloudinary setup - create product route - featured product route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import jwt from "jsonwebtoken";
 
 import User from "./models/user.model.js";
 import Product from "./models/product.model.js";
+import cloudinary from "./lib/cloudinary.js";
 
 
 
@@ -201,6 +202,67 @@ app.get("/api/products/", protectRoute, adminRoute, async (req, res) =>{ //This 
 		res.json({ products });
 	} catch (error) {
 		console.log("Error in getAllProducts controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+});
+
+
+app.get("/api/products/featured", async (req, res) => {
+  try {
+		let featuredProducts = await redis.get("featured_products"); // check if the featured products are stored in redis
+		if (featuredProducts) {                           
+			return res.json(JSON.parse(featuredProducts));    // if the featured products are stored in redis, return the products
+      //This improves response speed and reduces MongoDB queries.
+		}
+
+		// if not in redis, fetch from mongodb
+		// .lean() is gonna return a plain javascript object instead of a mongodb document
+		// which is good for performance
+		featuredProducts = await Product.find({ isFeatured: true }).lean(); // will only get triggered when if statement above is not fulfilled
+
+		if (!featuredProducts) {
+			return res.status(404).json({ message: "No featured products found" });
+		}
+
+		// store in redis for future quick access
+
+		await redis.set("featured_products", JSON.stringify(featuredProducts)); //Saves the fetched products in Redis (as a JSON string).
+    //The key is "featured_products".
+    //This is done so that the next time the route is accessed, the products can be fetched from Redis instead of MongoDB.
+    //This improves response speed and reduces MongoDB queries.
+
+		res.json(featuredProducts);
+	} catch (error) {
+		console.log("Error in getFeaturedProducts controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+});
+
+
+
+app.post("/api/products/", protectRoute, adminRoute, async (req, res) => { //This route is used to create a new product in the database.
+
+
+  try {
+		const { name, description, price, image, category } = req.body;
+
+		let cloudinaryResponse = null;
+
+		if (image) {
+			cloudinaryResponse = await cloudinary.uploader.upload(image, { folder: "products" }); //Uploads the image to Cloudinary.
+		}
+
+		const product = await Product.create({ //Creates a new product in the database and returns the product data.
+			name,
+			description,
+			price,
+			image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "", 
+			category,
+		});
+
+		res.status(201).json(product);
+	} catch (error) {
+		console.log("Error in createProduct controller", error.message);
 		res.status(500).json({ message: "Server error", error: error.message });
 	}
 });
